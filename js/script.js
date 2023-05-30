0b8244830868470b6af6caeb5642e4fb908b82e0
Elizeu Vito Santos <94203670+ElizeuV@users.noreply.github.com>
2023-05-30 12:05:40 -0300

    atualizações daniel

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const headerNavbar = document.getElementById('categories');
+let showNavbar = false;
+
+function toggleNavbar() {
+    showNavbar = !showNavbar;
+    
+    if(showNavbar) {
+        headerNavbar.style.marginLeft = '-0vw';
+        headerNavbar.style.animationName = 'showNavbar';
+    } else {
+        headerNavbar.style.marginLeft = '-100vw';
+        headerNavbar.style.animationName = 'closeNavbar';
+    }
+}
+
+function closeNavbar() {
+    if(showNavbar){
+        toggleNavbar();
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 900 && showNavbar) {
+        toggleNavbar();
+    }
+})
